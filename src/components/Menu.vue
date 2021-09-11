cafeb027e4d80052940a5d22aaf867b065671d2a
Cauê Melo <cauesmelo@gmail.com>
2021-09-11 17:28:37 -0300

    feat: add vue-router

---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+<div class="nav">
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Home page</h1>
+</template>
+
+<style scoped>
+</style>
