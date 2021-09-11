addb4d40b2e5b91bd1ca3dd69ba3c7dac143f09e
Cauê Melo <cauesmelo@gmail.com>
2021-09-11 17:38:51 -0300

    chore: add eslint prettier

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,12 +2,14 @@
 </script>
 
 <template>
-
-<div class="nav">
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-</div>
-
+  <div class="nav">
+    <router-link to="/">
+      Home
+    </router-link>
+    <router-link to="/about">
+      About
+    </router-link>
+  </div>
 </template>
 
 <style scoped>
