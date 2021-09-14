38cb49eb247e65014cee02acfcd783e089cd2296
Cauê Melo <cauesmelo@gmail.com>
2021-09-13 21:35:03 -0300

    feat: add pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,52 @@
+<script setup="props" lang="ts">
+  import { defineProps, ref } from 'vue';
+
+  const ITEMS_PER_PAGE = 10;
+
+  interface Props {
+    current: number;
+    total: number;
+  }
+
+  defineProps<Props>();
+
+  const calcPages = (total: number) => Math.ceil(total / ITEMS_PER_PAGE);
+</script>
+
+<template>
+  <div class="paginationComponentContainer">
+    <div :class="{ disable: current === 1 }">&lt;&lt;</div>
+    <div v-show="current > 3">
+      {{ current - 3 }}
+    </div>
+    <div v-show="current > 2">
+      {{ current - 2 }}
+    </div>
+    <div v-show="current > 1">
+      {{ current - 1 }}
+    </div>
+    <div>{{ current }}</div>
+    <div v-show="calcPages(total) > current">
+      {{ current - 3 }}
+    </div>
+    <div v-show="calcPages(total) > current + 1">
+      {{ current - 2 }}
+    </div>
+    <div v-show="calcPages(total) > current + 2">
+      {{ current - 1 }}
+    </div>
+    <div :class="{ disable: current === calcPages(total) }">&gt;&gt;</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .paginationComponentContainer {
+    display: flex;
+
+    > div {
+      cursor: pointer;
+      text-align: center;
+      min-width: 2rem;
+    }
+  }
+</style>
