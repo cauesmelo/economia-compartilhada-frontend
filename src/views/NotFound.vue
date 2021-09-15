59b66972f9078946cf7848036801540764a25d01
Cauê Melo <cauesmelo@gmail.com>
2021-09-15 18:31:03 -0300

    feat: add not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,52 @@
 <script setup lang="ts">
+  import Navigation from '@/components/Navigation.vue';
 </script>
 
 <template>
-  <h2>Página não encontrada.</h2>
+  <Navigation />
+  <div class="center">
+    <div class="container">
+      <div class="titleNotFound">
+        <div class="iconNotFound"></div>
+        <h2>Página não encontrada</h2>
+      </div>
+      <router-link to="/listar-itens" class="clickable">
+        Ir para itens
+      </router-link>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .center {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .container {
+      max-width: 1280px;
+      margin: 1rem;
+      margin-top: 3rem;
+      flex-grow: 1;
+      text-align: center;
+
+      .titleNotFound {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        h2 {
+          margin: 2rem 0.5rem;
+        }
+
+        .iconNotFound {
+          height: 2rem;
+          width: 2rem;
+          background-image: url('../assets/icons/black-Danger.svg');
+          background-size: cover;
+          background-position: center;
+        }
+      }
+    }
+  }
 </style>
